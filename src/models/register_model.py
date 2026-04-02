32a4f2ce41efd7504e0dfb40928ac524e50f5303
saurabhh1409 <saurabhnishad1409@gmail.com>
2026-04-02 23:38:38 +0530

    added register model

---
+++ src/models/register_model.py
@@ -0,0 +1,79 @@
+import mlflow
+import dagshub
+import json
+from pathlib import Path
+from mlflow import MlflowClient
+import logging
+import mlflow.client
+
+
+# create logger
+logger = logging.getLogger("register_model")
+logger.setLevel(logging.INFO)
+
+# console handler
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)
+
+# add handler to logger
+logger.addHandler(handler)
+
+# create a formatter
+formatter = logging.Formatter(fmt='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# initialize dagshub
+dagshub.init(repo_owner='saurabhh1409',
+             repo_name='swiggy-delivery-time-prediction', 
+             mlflow=True)
+
+# set the mlflow tracking server
+mlflow.set_tracking_uri("https://dagshub.com/saurabhh1409/swiggy-delivery-time-prediction.mlflow")
+
+
+def load_model_information(file_path):
+    with open(file_path) as f:
+        run_info = json.load(f)
+    return run_info
+
+
+if __name__ == "__main__":
+    # root path
+    root_path = Path(__file__).parent.parent.parent
+    
+    # run information file path
+    run_info_path = root_path / "run_information.json"
+    
+    # load run info
+    run_info = load_model_information(run_info_path)
+    
+    # get the run id, model name and artifact path
+    run_id = run_info["run_id"]
+    model_name = run_info["model_name"]
+    artifact_path = run_info["artifact_path"]
+    
+    logger.info(f"Run ID: {run_id}")
+    logger.info(f"Model Name: {model_name}")
+    
+    # use full artifact path instead of runs:/ URI
+    model_registry_path = f"{artifact_path}/{model_name}"
+    logger.info(f"Registering model from: {model_registry_path}")
+    
+    # register the model
+    model_version = mlflow.register_model(model_uri=model_registry_path,
+                                          name=model_name)
+    
+    # get the model version
+    registered_model_version = model_version.version
+    registered_model_name = model_version.name
+    logger.info(f"The latest model version in model registry is {registered_model_version}")
+    
+    # update the stage of the model to staging
+    client = MlflowClient()
+    client.transition_model_version_stage(
+        name=registered_model_name,
+        version=registered_model_version,
+        stage="Staging"
+    )
+    
+    logger.info("Model pushed to Staging stage")
